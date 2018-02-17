41787aedc91628b4cf6ed3c16917142e7291a0a3
Pubudu Welagedara <pwelagedara@virtusapolaris.com>
2018-02-17 08:18:54 -0500

    first commit

---
+++ src/main/java/com/example/microservice/MicroserviceApplication.java
@@ -0,0 +1,15 @@
+package com.example.microservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Created by pubudu on 2/17/18.
+ */
+@SpringBootApplication
+public class MicroserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroserviceApplication.class, args);
+	}
+}
---
+++ src/main/java/com/example/microservice/controller/SampleController.java
@@ -0,0 +1,9 @@
+package com.example.microservice.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class SampleController {
+
+
+}
