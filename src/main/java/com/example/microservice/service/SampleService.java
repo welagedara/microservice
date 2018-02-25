9f5fc0c40ba7c86b6eba78c732651c2a8c0d2a1a
pwelagedara <pubuduwelagedara@gmail.com>
2018-02-24 21:30:55 -0500

    testing deployment

--- src/main/java/com/example/microservice/service/SampleService.java
+++ src/main/java/com/example/microservice/service/SampleService.java
@@ -16,10 +16,11 @@ public class SampleService {
     private GreetingRepository greetingRepository;
 
     public MessageDto sayHello() {
-        List<Greeting> greetings = greetingRepository.findAll();
-
-        // Pick a random greeting
-        Random random = new Random();
-        return new MessageDto(greetings.get(random.nextInt(greetings.size())).getMessage());
+        return new MessageDto("this works");
+//        List<Greeting> greetings = greetingRepository.findAll();
+//
+//        // Pick a random greeting
+//        Random random = new Random();
+//        return new MessageDto(greetings.get(random.nextInt(greetings.size())).getMessage());
     }
 }
