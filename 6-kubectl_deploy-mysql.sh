b18922ce40a8dc15bbc4ff5041ed4d8476d16650
pwelagedara <pubuduwelagedara@gmail.com>
2018-03-12 22:44:04 -0400

    comment

--- 6-kubectl_deploy-mysql.sh
+++ 6-kubectl_deploy-mysql.sh
@@ -3,7 +3,7 @@
 # Merge all Database Scripts into one
 cat ./kubectl/mysql/database.sql ./src/main/resources/db/schema.sql ./src/main/resources/db/data.sql > ./kubectl/mysql/db.sql
 
-# Config Map for Data. (Note Looks like ConfigMaps can have many files as key value pairs. No folders though. You can write this bit of code using that feature too)
+# Config Map for Data. (Note: Looks like ConfigMaps can have many files as key value pairs. No folders though. You can write this bit of code using that feature too)
 kubectl create configmap mysql-initdb-config --from-file=./kubectl/mysql/db.sql
 
 # Secrets
